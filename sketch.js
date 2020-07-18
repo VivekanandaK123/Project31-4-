19cc0a1016af6516d1b17e34103c3eb6fb2d5b63
VivekanandaK123 <vivekanandakatakam16@gmail.com>
2020-07-18 17:33:49 +0530

    mallesham

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
+var engine,world;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  var canvas = createCanvas(480,800);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+ 
+ ground = new Ground(240,790,480,20);
+
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(255,255,255); 
+  rectMode(CENTER);
+  
+  
+
+  ground.display();
+
 }
